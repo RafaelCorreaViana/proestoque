c2a05de69a7a80b28c91ca44ec0105087fd9ce9b
Rafael Correa Viana <rafaelcv100@gmail.com>
2026-06-25 18:46:50 -0400

    chore: configure EAS Build and android package name

--- app.config.js
+++ app.config.js
@@ -3,6 +3,10 @@ module.exports = {
     ...require("./app.json").expo,
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL ?? "http://localhost:3333/api",
+      eas: {
+        projectId: "fa1b461c-5e35-4aa8-9562-642bed56dba2"
+      }
     },
   },
 };
+
